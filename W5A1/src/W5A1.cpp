a71f38aa7913b41fc0f11c5bbb56b76c4e3d498f
andersonht365 <andersonht365@gmail.com>
2016-09-29 21:20:39 -0400

    W5A1 start

---
+++ W5A1/src/W5A1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : W5A1.cpp
+// Author      : Anderson Thomas
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
